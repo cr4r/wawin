59a449e68e0347eb05bb8ba40bf88f7e5082b344
cr4r <cr4rrr@gmail.com>
2020-11-30 06:10:32 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wawin
 Bot Whatsapp Khusus Windows
+
+## Bahan Instalasi
+* [`nodejs`](https://nodejs.org/dist/v14.15.1/node-v14.15.1-x64.msi)
+* [`nmap`](https://nmap.org/dist/nmap-7.91-setup.exe)
+* [`wget`](https://sourceforge.net/projects/gnuwin32/files/wget/1.11.4-1/wget-1.11.4-1-setup.exe/download)
+* [`Tutorial Install wget`](https://www.addictivetips.com/windows-tips/install-and-use-wget-in-windows-10/)
---
+++ index.js
@@ -0,0 +1,54 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
+const msgHandler = require('./msgHndlr')
+const options = require('./options')
+
+const start = async (client = new Client()) => {
+        console.log('[SERVER] Bot Whatsapp sudah mulai!')
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[CLIENT]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+            client.getAmountOfLoadedMessages().then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
+
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+        }))
+
+        // Hilangkan tanda // jika ingin mengaktifkan fiturnya
+        //fitur out otomatis
+        client.onAddedToGroup(((chat) => {
+            client.sendText(chat.id, `Halo om, kalo mau masukin saya (bot) ke grub, silahkan hubungi wa.me/6282237416678. eeittts gak gratis ya :) (donasi seiklasnya min.5k) bye bye :p`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
+            // let totalMem = chat.groupMetadata.participants.length
+            // if (totalMem < 30) { 
+            // 	client.sendText(chat.id, `Cih member nya cuma ${totalMem}, Kalo mau invite bot, minimal jumlah mem ada 30`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
+            // } else {
+            //     client.sendText(chat.groupMetadata.id, `Halo warga grup *${chat.contact.name}* terimakasih sudah menginvite bot ini, untuk melihat menu silahkan kirim *!help*`)
+            // }
+        }))
+
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        // client.onIncomingCall(( async (call) => {
+        //     await client.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block!')
+        //     .then(() => client.contactBlock(call.peerJid))
+        // }))
+    }
+
+create('CR4R', options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
